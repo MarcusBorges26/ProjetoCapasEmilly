0a435cdd5ec2240f58bedc545d469761d9ce55b7
Marcus <103702779+MarcusBorges26@users.noreply.github.com>
2023-04-29 11:51:24 -0300

    :glass:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,14 @@
-import { Component, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Router, RouterModule, Routes } from '@angular/router';
-import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
-import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
-import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
-import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
-import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
-import { AuthGuard } from './auth.guard';
-import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
 const routes: Routes = [
-    {path:"primeira-pagina", component: PrimeiraPaginaComponent},
-    {path: "segunda-pagina", component: SegundaPaginaComponent},
-    {path: "", redirectTo: "primeira-pagina", pathMatch: "full"},
-    {path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent},
-    {path: "login", component: LoginComponent},
-    {path: "pagina-protegida", component: PaginaProtegidaComponent, canActivate:[AuthGuard]},
-    { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
-    {path: "**", component: PaginaNaoEncontradaComponent}
-    
-]
+  { path: 'Produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
+  {path: "", redirectTo: "Produtos", pathMatch: "full" },
+  {path: "**", component: NaoEncontradoComponent}];
 
 @NgModule({
-  declarations: [],
   imports: [RouterModule.forRoot(routes)],
-  exports:[RouterModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
-import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
-import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
-import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
-import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
-import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimeiraPaginaComponent,
-    SegundaPaginaComponent,
-    PaginaNaoEncontradaComponent,
-    PaginaComParametrosComponent,
-    PaginaProtegidaComponent,
-    LoginComponent
+    HeaderComponent,
+    FooterComponent,
+    NaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
