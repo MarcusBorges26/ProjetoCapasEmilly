36dfbd6da41a0a7d3640002c87821fa60d181c4b
Marcus <103702779+MarcusBorges26@users.noreply.github.com>
2023-05-03 12:34:24 -0300

    :memo:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,29 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: 'Produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
-  {path: "", redirectTo: "Produtos", pathMatch: "full" },
-  {path: "**", component: NaoEncontradoComponent}];
+    {path:"primeira-pagina", component: PrimeiraPaginaComponent},
+    {path: "segunda-pagina", component: SegundaPaginaComponent},
+    {path: "", redirectTo: "primeira-pagina", pathMatch: "full"},
+    {path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent},
+    {path: "login", component: LoginComponent},
+    {path: "pagina-protegida", component: PaginaProtegidaComponent, canActivate:[AuthGuard]},
+    { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
+    {path: "**", component: PaginaNaoEncontradaComponent}
+    
+]
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
